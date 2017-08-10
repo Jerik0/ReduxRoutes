47c0762e6e31fe960c8ab4d52e36e2cec6f5c774
Jeriko <jcarrico1985@live.com>
2017-08-10 12:55:39 -0500

    remove test routes hello and goodbye. add ajax request to retrieve posts to view automatically. add test post to view. remove app.js.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
-}
-
-class GoodBye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello}/>
-        <Route path="/goodbye" component={GoodBye}/>
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
